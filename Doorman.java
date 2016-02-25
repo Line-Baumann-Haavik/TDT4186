b145463f82ecd7e3552b02442477fe8681ba3037
ailhay <aileenh@stud.ntnu.no>
2016-02-25 11:47:04 +0100

    Update Doorman.java

--- Doorman.java
+++ Doorman.java
@@ -2,29 +2,66 @@
  * This class implements the doorman's part of the
  * Barbershop thread synchronization example.
  */
-public class Doorman {
+public class Doorman extends Thread{
 	/**
 	 * Creates a new doorman.
 	 * @param queue		The customer queue.
 	 * @param gui		A reference to the GUI interface.
 	 */
+	CustomerQueue queue;
+	Gui gui;
+	
+	
 	public Doorman(CustomerQueue queue, Gui gui) { 
-		// Incomplete
+		this.queue = queue;
+		this.gui = gui;
+		
 	}
 
 	/**
 	 * Starts the doorman running as a separate thread.
 	 */
 	public void startThread() {
-		// Incomplete
+		super.start();
 	}
 
 	/**
 	 * Stops the doorman thread.
 	 */
 	public void stopThread() {
-		// Incomplete
+		super.stop();
 	}
+	
+	/**
+	 * Adds a new customer to the queue.
+	 */
+	private void addCustomer(){
+		boolean k = queue.addCustomer(new Customer());
+		
+	}
+	
+	/**
+	 * Makes the doorman(thread) go to sleep.
+	 */
+	private void sleep(){
+		int min = Constants.MIN_DOORMAN_SLEEP;
+		int max = Constants.MAX_DOORMAN_SLEEP;
+		int r = min+(int)(Math.random()*(max-min+1));
+		
+		try {
+			super.sleep(r);
+		} catch (InterruptedException e) {
+			
+		}
+	}
+	
 
-	// Add more methods as needed
+	public void run(){
+		while(true){
+			addCustomer();
+			sleep();
+		}
+	}
+	
+	
 }
