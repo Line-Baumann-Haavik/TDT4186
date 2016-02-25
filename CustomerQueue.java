22e1544f3949217b5d26857ce9609c14e50411a9
johansme <johansme@users.noreply.github.com>
2016-02-25 11:58:11 +0100

    Added files via upload

--- CustomerQueue.java
+++ CustomerQueue.java
@@ -2,14 +2,51 @@
  * This class implements a queue of customers as a circular buffer.
  */
 public class CustomerQueue {
+	
+	private Gui gui;
+	private int nextCustomerSeat;
+	private int oldestCustomer;
+	private int queueLength;
+	private Customer[] queue;
+	
 	/**
 	 * Creates a new customer queue.
 	 * @param queueLength	The maximum length of the queue.
 	 * @param gui			A reference to the GUI interface.
 	 */
     public CustomerQueue(int queueLength, Gui gui) {
-		// Incomplete
+    	this.queueLength = queueLength;
+    	queue = new Customer[queueLength];
+    	this.nextCustomerSeat = 0;
+    	this.oldestCustomer = 0;
+    	this.gui = gui;
 	}
 
-	// Add more methods as needed
+	public boolean addCustomer(Customer customer) {
+		if (queue[nextCustomerSeat] != null) {
+			return false;
+		}
+		queue[nextCustomerSeat] = customer;
+		gui.fillLoungeChair(nextCustomerSeat, customer);
+		nextCustomerSeat++;
+		if (nextCustomerSeat == queueLength) {
+			nextCustomerSeat = 0;
+		}
+		return true;
+	}
+	
+	public Customer popCustomer(int pos) {
+		if (queue[oldestCustomer] == null) {
+			return null;
+		}
+		gui.emptyLoungeChair(oldestCustomer);
+		Customer customer = queue[oldestCustomer];
+		queue[oldestCustomer] = null;
+		oldestCustomer++;
+		if (oldestCustomer == queueLength) {
+			oldestCustomer = 0;
+		}
+		gui.fillBarberChair(pos, customer);
+		return customer;
+	}
 }
