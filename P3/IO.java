ce9565d00372ff27c02fce5974de732a0bc8f666
Line Baumann Haavik <lbhaavik@online.no>
2016-04-13 13:39:14 +0200

    oppdatert IO

--- P3/IO.java
+++ P3/IO.java
@@ -12,6 +12,27 @@ public class IO implements Constants{
 		
 	}
 	
+	public Event addIOrequest(Process requestingProcess, long clock){
+		ioQueue.insert(requestingProcess); 
+		requestingProcess.calculateTimeToNextIoOperation(); 
+	}
+	
+	public Event startIoOperation(long clock) { 
+		if(activeProcess == null) { 
+		// The device is free 
+			if(!ioQueue.isEmpty()) { 
+				// Let the first process in the queue start I/O -----
+				// Update statistics 
+				statistics.nofProcessedIoOperations++;
+				// Calculate the duration of the I/O operation and return the END_IO event ------
+				return new Event(END_IO, clock + ioOperationTime); 
+		
+			}
+		}
+		// else no process are waiting for I/O
+		// else another process is already doing I/O
+	}
+	
 	public void timePassed(long timePassed) {
 		statistics.IOQueueLengthTime += IOqueue.getQueueLength()*timePassed;
 		if (IOqueue.getQueueLength() > statistics.IOQueueLargestLength) {
